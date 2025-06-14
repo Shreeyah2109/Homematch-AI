8e7e9e674bbd9839e4038aae7798843653724aaf
Yashraj Kalshetti <124449343+Yashrajgithub@users.noreply.github.com>
2025-06-14 21:46:54 +0530

    Update ResetPassword.tsx

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -68,7 +68,7 @@ export default function ResetPassword() {
       }
 
       try {
-        const response = await fetch(`http://localhost:5000/api/users/reset-password/${token}`);
+        const response = await fetch(`https://homematch-ai.onrender.com/api/users/reset-password/${token}`);
         const data = await response.json();
 
         if (response.ok) {
